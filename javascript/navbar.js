9df812b7cf504455edf95c378024570e4374ae24
Logan Pageler <pagelerlogan@gmail.com>
2023-01-13 18:45:09 -0700

    fixed js

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,6 +1,6 @@
 var navbar = document.getElementById("navbar")
 var main = document.getElementById("main")
-
+this.print("hello")
 this.window.addEventListener("scroll", function() {
     this.print("hello")
     if(this.scrollY > this.window.innerHeight) {
