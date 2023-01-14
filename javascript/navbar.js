214396806890d630fb92d0fd0c48e561c7eedcb5
Logan Pageler <pagelerlogan@gmail.com>
2023-01-13 21:55:29 -0700

    fixed js fr

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,9 +1,9 @@
 var navbar = document.getElementById("navbar");
-var main = document.getElementById("main");
+
 console.log("hello");
 this.window.addEventListener("scroll", function() {
     console.log("hello");
-    if(this.scrollY > this.window.innerHeight) {
+    if(this.scrollY > this.window.innerHeight * 0.82) {
         navbar.className = "navbar-fixed";
     } else {
         navbar.className = "navbar-scroll";
