15659c1765f428bc07190d4013bdc1818eab74e7
Logan Pageler <pagelerlogan@gmail.com>
2023-02-10 16:41:03 -0700

    added images

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,11 +1,9 @@
 var navbar = document.getElementById("navbar");
 
-console.log("hello");
-this.window.addEventListener("scroll", function() {
-    console.log("hello");
-    if(this.scrollY > this.window.innerHeight * 0.82) {
-        navbar.className = "navbar-fixed";
-    } else {
-        navbar.className = "navbar-scroll";
-    }
-})
+this.window.addEventListener("scroll", function () {
+	if (this.scrollY > this.window.innerHeight - 80) {
+		navbar.className = "navbar-fixed";
+	} else {
+		navbar.className = "navbar-scroll";
+	}
+});
